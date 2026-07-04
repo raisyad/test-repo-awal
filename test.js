1f8f86f2de7f9f21e1b3378df9a9ff74b265bb4a
Syncz <raisyadjullfikar@gmail.com>
2026-07-04 11:03:09 +0700

    file kedua

---
+++ test.js
@@ -0,0 +1,12 @@
+let queue = [];
+
+queue.push("Baju 1");
+queue.push("Baju 2");
+queue.push("Baju 3");
+
+console.log("\nSebelum QUEUE")
+console.log(queue)
+
+console.log("\nSesudah QUEUE")
+queue.shift();
+console.log(queue)
